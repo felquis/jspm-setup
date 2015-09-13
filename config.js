ad056aa8cc4d49ba0af9dfa4fe1ae72f6bdd60cf
Felquis Gimenes <frgformenton@gmail.com>
2015-09-13 15:29:40 -0300

    Add jspm Hello World

---
+++ config.js
@@ -0,0 +1,44 @@
+System.config({
+  defaultJSExtensions: true,
+  transpiler: "babel",
+  babelOptions: {
+    "optional": [
+      "runtime",
+      "optimisation.modules.system"
+    ]
+  },
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
+  },
+  bundles: {
+    "build.js": [
+      "lib/main.js",
+      "lib/helloWorld.js",
+      "github:components/jquery@2.1.4",
+      "github:components/jquery@2.1.4/jquery"
+    ]
+  },
+
+  map: {
+    "babel": "npm:babel-core@5.8.24",
+    "babel-runtime": "npm:babel-runtime@5.8.24",
+    "core-js": "npm:core-js@1.1.4",
+    "jquery": "github:components/jquery@2.1.4",
+    "lodash-node": "npm:lodash-node@3.10.1",
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "npm:babel-runtime@5.8.24": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:core-js@1.1.4": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:lodash-node@3.10.1": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    }
+  }
+});
